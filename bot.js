683a37344371c204c2d43c50a27df14dfa535702
xukeler <1401525569@qq.com>
2020-10-13 21:15:18 +0800

    no message

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ActivityHandler, MessageFactory } = require('botbuilder');
+const { ActivityHandler, MessageFactory ,CardFactory,ActivityTypes} = require('botbuilder');
 
 class EchoBot extends ActivityHandler {
     constructor() {
@@ -13,13 +13,19 @@ class EchoBot extends ActivityHandler {
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
-
         this.onMembersAdded(async (context, next) => {
+            const reply = { type: ActivityTypes.Message };
             const membersAdded = context.activity.membersAdded;
-            const welcomeText = 'Hello and welcome!';
+            const welcomeText = 'Welcome to kloud Ai!';
+            const card =  CardFactory.signinCard(
+                'Sign in Kloud',
+                'https://kloud.cn',
+            );
+            reply.attachments = [card];
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
+                    await context.sendActivity(welcomeText);
+                    await context.sendActivity(reply);
                 }
             }
             // By calling next() you ensure that the next BotHandler is run.
