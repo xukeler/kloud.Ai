fe9841f87c83b7cb08462b5734a5d4316366fe0b
xukeler <1401525569@qq.com>
2020-10-15 09:36:18 +0800

    测试没有绑定发送登录信息

--- bot.js
+++ bot.js
@@ -22,10 +22,11 @@ class EchoBot extends ActivityHandler {
                 'https://testkloudsync.peertime.cn/login?'+context.activity.channelId+'='+context.activity.from.id,
             );
             reply.attachments = [card];
+            await context.sendActivity(welcomeText);
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
                     await context.sendActivity(welcomeText);
-                    await context.sendActivity(reply);
+                    // await context.sendActivity(reply);
                 }
             }
             // By calling next() you ensure that the next BotHandler is run.
