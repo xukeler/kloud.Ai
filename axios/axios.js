9f95a9e4722994c0bfff1021158fe358a4368c5e
xukeler <1401525569@qq.com>
2020-10-15 17:43:05 +0800

    绑定session

---
+++ axios/axios.js
@@ -0,0 +1,55 @@
+const axios =require('axios') 
+axios.defaults.timeout = 20000;
+axios.defaults.baseURL = 'https://testapi.peertime.cn/peertime/V1/';
+let Webapi={
+    getAjax(url){
+        return new Promise((resolve, reject) => {
+            axios.get(url,null,{ responseType: "json" }).then(res =>
+            {
+                resolve(res.data);
+            }).catch(err =>
+            {
+                console.log(err);
+                resolve(null);
+            });
+        });
+    },
+    postAjax(url,obj)
+    {
+        return new Promise((resolve, reject) => {
+            axios.post(url,JSON.stringify(obj)).then(res =>
+            {
+                resolve(res.data);
+            }).catch(err =>
+            {
+                resolve(null);
+            });
+        });
+    },
+    setToken(token){
+        axios.defaults.headers.common['Authorization'] = token;
+    },
+    async getSkypeToken(id){
+        let url ='User/TokenBySkypeSessionID?id='+id;
+        let result=await this.getAjax(url,obj);
+        if (!result)
+        {
+            return null;
+        } else if(result&&result.RetCode==0&&result.RetData){
+            return result.RetData;
+        }
+    },
+    async getTeamsToken(id){
+        let url ='User/TokenByTeamsSessionID?id='+id;
+        let result=await this.getAjax(url);
+        let res_Json=eval('(' + result + ')')
+        if (!res_Json)
+        {
+            return null;
+        } else if(res_Json&&res_Json.RetCode==0) {
+            return res_Json.RetData
+        }
+    },
+
+}
+module.exports.Webapi=Webapi
--- bot.js
+++ bot.js
@@ -1,22 +1,41 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ActivityHandler, MessageFactory ,CardFactory,ActivityTypes} = require('botbuilder');
-
+const { ActivityHandler, MessageFactory ,CardFactory,ActivityTypes,ActionTypes} = require('botbuilder');
+const {Webapi}=require('./axios/axios')
 class EchoBot extends ActivityHandler {
     constructor() {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            const replyText = `Echo: ${ context.activity.text }`;
-            await context.sendActivity(context.activity.channelId);
-            // By calling next() you ensure that the next BotHandler is run.
+            let res="";
+            if(context.activity.channelId=="skype"){
+                res= await Webapi.getSkypeToken(context.activity.from.id);
+            }else{
+                res= await Webapi.getTeamsToken(context.activity.from.id);
+            }
+            
+            if(res){
+                const replyText = `Echo: ${ context.activity.text }`;
+                await context.sendActivity(replyText);
+            }else{
+                const reply = { type: ActivityTypes.Message };
+                const card =  CardFactory.signinCard(
+                    'Sign in Kloud',
+                    'https://testkloudsync.peertime.cn/login?'+context.activity.channelId+'='+context.activity.from.id,
+                    "Your identity information has not been bound to Kloud, please log in to KLoud and bind it before use."
+                    );
+                reply.attachments = [card]; 
+                await context.sendActivity(reply);
+            }
+
             await next();
         });
         this.onMembersAdded(async (context, next) => {
             const reply = { type: ActivityTypes.Message };
             const membersAdded = context.activity.membersAdded;
             const welcomeText = 'Welcome to kloud.ai, Please login!';
+            await context.sendActivity(reply);
             const card =  CardFactory.signinCard(
                 'Sign in Kloud',
                 'https://testkloudsync.peertime.cn/login?'+context.activity.channelId+'='+context.activity.from.id,
